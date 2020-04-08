7efd766b3785b5580e77cc2f32f8504de15a8b6b
Georgiana <stativa50@gmail.com>
2020-04-08 23:24:03 +0300

    Added database

---
+++ databaseCon.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const startConnection = async function () {
+  try {
+    await mongoose.connect(process.env.DB_CON, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Succesful connection to database");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = startConnection;
